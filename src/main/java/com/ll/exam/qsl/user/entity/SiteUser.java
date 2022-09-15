682900c2d2c82a4760bc4cd7455b5e8f0a567eea
jeonga <jjeong0106@gmail.com>
2022-09-15 15:45:36 +0900

    세팅

---
+++ src/main/java/com/ll/exam/qsl/user/entity/SiteUser.java
@@ -0,0 +1,34 @@
+package com.ll.exam.qsl.user.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// @Entity는 JPA를 사용해 테이블과 매핑할 클래스에 붙여주는 어노테이션이다. 객체와 테이블 매핑.
+// 이 어노테이션을 붙임으로써 JPA가 해당 클래스를 관리하게 된다.
+@Entity
+// @Getter , @Setter (접근자/설정자 생성)
+// @Getter @Setter 어노테이션을 붙이주면 명클래스 멤버 변수들의 getter, setter 메소드를 이용 할 수 있다.
+@Getter
+@Setter
+public class SiteUser {
+    //기본키 매핑
+    // 테이블 상의 Primary Key 와 같은 의미를 가지며 @Id 어노테이션으로 표기
+    @Id
+    // 기본 키 생성을 데이터베이스에 위임.
+    // 즉, id 값을 null로 하면 DB가 알아서 AUTO_INCREMENT 해준다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 필드와 컬럼 매핑
+    // DB에 영향을 주는 것
+    //해당 필드는 unique 함을 의미
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
